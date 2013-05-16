4b084d63cfb2b92f90520732094a4c632c012d41
Christian Javellana <cjjavellana@gmail.com>
2013-05-16 09:11:13 +0800

    Separated impl from interface for aes file encryption

---
+++ FileCrypt/aes_file_encryption.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "files.h"
+#include "aes.h"
+#include "modes.h"
+
+
+using namespace CryptoPP;
+using namespace std;
+
+namespace filecrypt
+{
+	namespace encryptors
+	{
+		class AesFileEncryptor
+		{
+		public:
+			AesFileEncryptor();
+			~AesFileEncryptor();
+			void EncryptFile(const char *fileToEncrypt, FileSink *pOutputFile, const byte *key, const byte *iv);
+			void DecryptFile(const char *fileToDecrypt, FileSink *pDecryptedFile, const byte *key, byte *iv);
+		private:
+			void ValidateKeyAndIvLength(const byte *key, const byte *iv);
+			bool FileExist(const char *location);
+		};
+	}
+}
